7a87191855a3b17fbab72fb21bea7671c9b41e41
Duc Hoang <duchoang.work2901@gmail.com>
2025-01-04 12:22:52 +0700

    12

--- codex.lua
+++ codex.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
